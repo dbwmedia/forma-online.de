7fdae8f88def9d3778a2b9057d103f1521ec56ab
Dennis Buchwald <buchwald95@icloud.com>
2025-03-28 11:00:43 +0100

    style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '1d0a63630f97998ebb77');
+<?php return array('dependencies' => array('jquery'), 'version' => '70bad5a02c6e3bbfe575');
